fabf8b79e8e29efc42e4b5ea9129b20b4d4b1d05
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-24 19:27:37 -0400

    created header and main files

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <nav>
+        <div className="logo">
+          <h3>The Legion Steam</h3>
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
