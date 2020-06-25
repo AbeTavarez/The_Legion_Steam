2d39f0df5fb1f86f744085fcc5a4719bb301a516
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-24 20:52:13 -0400

    header layout

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 
+import "./Header.css";
+import car_img from "../../assets/images/car_img.jpg";
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <div className="header-container">
       <nav>
-        <div className="logo">
-          <h3>The Legion Steam</h3>
-        </div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+        <ul className="nav-bar">
+          <Link className="s-link">Home</Link>
+          <Link className="s-link">About</Link>
+          <Link className="s-link">Contact</Link>
         </ul>
       </nav>
+      <div className="img-container">
+        <img src={car_img} alt="header-image" />
+      </div>
+
+      <div className="logo">
+        <div>The Legion Steam</div>
+      </div>
     </div>
   );
 }
