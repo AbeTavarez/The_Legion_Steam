03a34c6595ea70ef69dd949017811d854ba10ac5
Abraham E. Tavarez <abetavarez@icloud.com>
2020-06-29 16:22:51 -0400

    jwt included in responce

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({user_id: @user.id, username: @user.username})
+      render json: {user: @user, tokem: @token}, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
