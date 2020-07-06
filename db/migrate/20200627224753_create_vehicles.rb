6bb01ac3825ae5abd57364a5bdbc136cadadaec7
Abraham E. Tavarez <abetavarez@icloud.com>
2020-07-06 13:03:15 -0400

    added references to models

--- db/migrate/20200627224753_create_vehicles.rb
+++ db/migrate/20200627224753_create_vehicles.rb
@@ -1,13 +1,13 @@
 class CreateVehicles < ActiveRecord::Migration[6.0]
   def change
     create_table :vehicles do |t|
-      t.string :brand
-      t.string :model
-      t.string :color
+      t.string :brand, null: false
+      t.string :model, null: false
+      t.string :color, null: false
       t.integer :year
       t.string :vehicle_type
-      t.string :vehicle_plate
-
+      t.string :vehicle_plate, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20200627230743_create_jobs.rb
+++ db/migrate/20200627230743_create_jobs.rb
@@ -1,13 +1,13 @@
 class CreateJobs < ActiveRecord::Migration[6.0]
   def change
     create_table :jobs do |t|
-      t.string :location
-      t.datetime :date
-      t.string :job_type
+      t.string :location, null: false
+      t.datetime :date, null: false
+      t.string :job_type, null: false
       t.string :special_request
       t.boolean :isAccepted
       t.boolean :need_access
-
+      t.references :vehicle, null: false, foreign_key: true
       t.timestamps
     end
   end
